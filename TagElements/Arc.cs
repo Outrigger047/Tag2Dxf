55c2cf65acd81a2f1ae5f936882760ada975d9a2
Andrew Wyshak <andrew.wyshak@gmail.com>
2023-11-20 19:12:53 -0500

    Forgot one header lol

--- TagElements/Arc.cs
+++ TagElements/Arc.cs
@@ -1,9 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
 /// <summary>
-/// Arc.cs
-/// Created 11/18/2023
-/// Andrew Wyshak
-/// Surface Creations of Maine
+/// Tag2Dxf
+/// (C) Copyright 2023 Surface Creations of Maine
+///
+/// File:    Arc.cs
+/// Created: November 20, 2023
+/// Purpose: Class to encapsulate a TAG Arc
+/// Author:  Andrew Wyshak
 /// </summary>
+///////////////////////////////////////////////////////////////////////////////
 
 namespace Tag2Dxf.TagElements
 {
