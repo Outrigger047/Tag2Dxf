4ad5f26086c070da51414d49e2f5f89f111ea6c8
Andrew Wyshak <andrew.wyshak@gmail.com>
2023-12-22 07:23:43 -0500

    Added file for converter

---
+++ Converter.cs
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+/// <summary>
+/// Tag2Dxf
+/// (C) Copyright 2023 Surface Creations of Maine
+///
+/// File:    Converter.cs
+/// Created: December 22, 2023
+/// Purpose: Static converter to consume a TAG file and output a DXF
+/// Author:  Andrew Wyshak
+/// </summary>
+///////////////////////////////////////////////////////////////////////////////
